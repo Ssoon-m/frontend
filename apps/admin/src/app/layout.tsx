75a06a31e7341ddb6abd4b04a8d44c6e0ed06f7b
순민 <tkksm2@gmail.com>
2023-05-22 23:29:56 +0900

    feat: web

--- apps/admin/src/app/layout.tsx
+++ apps/admin/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <div>as</div>
+      <div>ADMIN!</div>
       <body className={inter.className}>{children}</body>
     </html>
   );
